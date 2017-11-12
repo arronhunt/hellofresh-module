e09ec9aa6f2ffbd50c49e02b8b8336f20ec57210
Arron Hunt <arronjhunt@gmail.com>
2017-11-11 16:56:01 -0800

    Documenting

--- src/index.js
+++ src/index.js
@@ -4,34 +4,62 @@ const HOST = 'gw.hellofresh.com'
 const BEARER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MTMwNjM1OTAsImp0aSI6IjFhNjhkMjk1LTk5OGEtNGYxMy1hNzVlLTljYTU3N2FjMDY0YiIsImlhdCI6MTUxMDQzMzg0NywiaXNzIjoic2VuZiJ9.fB7kf4-p93lucuqfGX2BItuqQoYBxi_YwhN9jIyLi-4';
 
 class Recipes {
+    /**
+     * List all the recipes available
+     * @param {number} page - Page through the results 
+     */
     static list(skip = 0) {
         return query('recipes', {skip});
     }
 
+    /**
+     * Retrieve details for a recepie with ID
+     * @param {string} id 
+     */
     static getById(id) {
         return query(`recipes/${id}`);
     }
 }
 
 class Menus {
+    /**
+     * List the menu for the current week
+     */
     static current() {
         return query('menus');
     }
 
+    /**
+     * Retrieve details for a menu with ID
+     * @param {string} id 
+     */
     static getById(id) {
         return query(`menus/${id}`);
     }
 
+    /**
+     * Retreive a weekly menu for a specific week and year
+     * @param {number} year 
+     * @param {number} week - Week number in a year, 0 - 52
+     */
     static week(year, week) {
         return query('menus', {week: `${year}-W${week}`});
     }
 }
 
 class Ingredients {
+    /**
+     * List all the ingredients available
+     * @param {number} page - Page through the results 
+     */
     static list(skip = 0) {
         return query('ingredients', {skip});
     }
 
+    /**
+     * Retrieve details for an ingredient by ID
+     * @param {string} id 
+     */
     static getById(id) {
         return query(`ingredients/${id}`);
     }
